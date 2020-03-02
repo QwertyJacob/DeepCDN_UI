fe7fe39e54ac2673013328a081d46293a1783cf9
Jesus <jesusfernando.cevallosmoreno@jc.elis.org>
2020-03-02 11:39:20 +0100

    Initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from dashboard.models import Order
+from django.core import serializers
+
+
+def pivot_data(request):
+    dataset = Order.objects.all()
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
+
+def dashboard_with_pivot(request):
+    return render(request, 'dashboard_with_pivot.html', {})
+
+def page_with_messages(request):
+
+
+    return render(request, 'page_enties.html')
